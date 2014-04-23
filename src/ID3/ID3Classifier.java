b9c503c9dcabc851719295b0073ee9198b23c2b1
jacob <jacobgdinesen@gmail.com>
2014-04-23 16:43:10 +0200

    test printout added

--- src/ID3/ID3Classifier.java
+++ src/ID3/ID3Classifier.java
@@ -1,9 +1,12 @@
 package ID3;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+
+import json.MatchDecoder;
 
 import analyser.Action;
 import analyser.Match;
@@ -15,6 +18,17 @@ public class ID3Classifier {
 	
 	ID3Tree tree;
 	public static int analyzeLength = 10;
+	
+	public static void main(String[] args){
+		
+		try {
+			List<Match> matches = new MatchDecoder().decode();
+			ID3Classifier id3c = new ID3Classifier(matches);
+		}
+		catch (IOException e) {e.printStackTrace();}
+		
+		
+	}
 	
 	public ID3Classifier(List<Match> trainingData){
 		List<String[]> matchArrays = convertToMatchArrays(trainingData);
@@ -28,17 +42,22 @@ public class ID3Classifier {
 	public List<String[]> convertToMatchArrays(List<Match> trainingData){
 		
 		ArrayList<String[]> matches = new ArrayList<String[]>();
+		System.out.println("trainingdata.size="+trainingData.size());
 		
 		for(Match m : trainingData){
 			String[] match = new String[analyzeLength+1];
-			Player[] p = m.getPlayers();
-			List<Action> p1a = p[0].getActions();
-			List<Action> p2a = p[1].getActions();
+			Player[] p = m.players;
+			if(p == null){
+				System.out.println("p is null");
+			}
+			System.out.println("p.length="+p.length);
+			List<Action> p1a = p[0].actions;
+			List<Action> p2a = p[1].actions;
 			
 			for(int i = 0; i < analyzeLength; i++){
 				match[i] = p1a.get(i).toString() + p2a.get(i).toString();
 			}
-			if(p[0].isWin()){
+			if(p[0].win){
 				match[analyzeLength] = "win";
 			}else{
 				match[analyzeLength] = "loss";
